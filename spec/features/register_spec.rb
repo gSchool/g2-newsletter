63684d42e6197925a68782f3f49d37a2f5f3bbd8
Keri Clowes <kerilyn.clowes@gmail.com>
2014-05-15 11:12:58 -0600

    [#71357322] user cannot register with a blank password
    Signed-off-by: Zach Klabunde <zbunde@gmail.com>

--- spec/features/register_spec.rb
+++ spec/features/register_spec.rb
@@ -16,6 +16,7 @@ feature 'Registering an account' do
     click_on 'Register'
     fill_in 'user[email]', :with => 'email@email.com'
     fill_in 'user[password]', :with => 'password'
+    fill_in 'user[password_confirmation]', :with => 'password'
     click_on 'register'
     expect(page).to have_content 'Welcome email@email.com'
     click_on 'Logout'
@@ -27,7 +28,19 @@ feature 'Registering an account' do
     click_on 'Register'
     fill_in 'user[email]', :with => '  '
     fill_in 'user[password]', :with => 'password'
+    fill_in 'user[password_confirmation]', :with => 'password'
     click_on 'register'
     expect(page).to have_content "Email can't be blank"
   end
+
+  scenario 'password cannot be blank when trying to register' do
+    visit '/'
+    click_on 'Register'
+    fill_in 'user[email]', :with => 'email@example.com'
+    fill_in 'user[password]', :with => '  '
+    fill_in 'user[password_confirmation]', :with => ' '
+    click_on 'register'
+    expect(page).to have_content "Password can't be blank"
+  end
+
 end
