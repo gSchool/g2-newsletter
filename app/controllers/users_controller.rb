561f497bf2db9e699c89303a2fba1495568c14de
Keri Clowes <kerilyn.clowes@gmail.com>
2014-05-15 10:16:32 -0600

    [#71357318] user can register for the site and logout
    Signed-off-by: Aubrey Howell <simplyaubs@gmail.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,18 +6,17 @@ class UsersController < ApplicationController
   def create
     email = params[:user][:email]
     password = params[:user][:password]
-
     @user = User.create(:email => email, :password => password)
-
-
-
     if @user.save
-
       session[:user_id] = @user.id
-
       redirect_to root_path, notice: "User Created"
     else
       render :new
     end
+  end
+
+  def logout
+    session.clear
+    redirect_to root_path, notice: "You are now logged out"
   end
 end
