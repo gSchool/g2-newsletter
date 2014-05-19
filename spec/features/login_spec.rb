0a27fa1ea73c66593cca10e4a825be0a303c0866
Aubrey <simplyaubs@gmail.com>
2014-05-19 16:35:13 -0600

    [#71357334] if a user attempts to login with incorrect username and password they recieve an error message
    Signed-off-by: Zach Klabunde <zbunde@gmail.com>

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -5,14 +5,14 @@ feature 'Login' do
   scenario 'a registered user can login with the credentials they supplied' do
     visit '/'
     click_on 'Register'
-    fill_in 'user[email]', :with => 'email@example.com'
+    fill_in 'user[email]', :with => 'email@email.com'
     fill_in 'user[password]', :with => 'password'
     fill_in 'user[password_confirmation]', :with => 'password'
     click_on 'Register'
     click_on 'Logout'
     visit '/'
     click_on 'Login'
-    fill_in 'email', :with => 'email@example.com'
+    fill_in 'email', :with => 'email@email.com'
     fill_in 'password', :with => 'password'
     click_on 'login'
     expect(page).to have_content "Welcome back"
@@ -27,6 +27,20 @@ feature 'Login' do
     click_on 'Logout'
     expect(page).to_not have_content 'Welcome to the newsletter application'
     expect(page).to have_content 'You have logged out.'
+  end
+  scenario 'a user cannot register with incorrect password' do
+    visit '/'
+    click_on 'Register'
+    fill_in 'user[email]', :with => 'email@email.com'
+    fill_in 'user[password]', :with => 'password'
+    fill_in 'user[password_confirmation]', :with => 'password'
+    click_on 'Register'
+    click_on 'Logout'
+    click_on 'Login'
+    fill_in 'email', :with => 'email@email.com'
+    fill_in 'password', :with => 'coolcool'
+    click_on 'login'
+    expect(page).to have_content 'Email or password incorrect'
   end
 
 end
