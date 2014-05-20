e863fc485f0f4e98f7c188e00819762d8cc55fd5
Zach Klabunde <zbunde@gmail.com>
2014-05-20 16:11:29 -0600

    [#71417406] fixed capitilization errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :email, :presence => true, :uniqueness => true
-  validates :email, format: {with:/[-0-9a-zA-Z.+_]+@[-0-9a-zA-Z.+_]+\.[a-zA-Z]{2,3}/, message: "Must be a valid email address" }
+  validates :email, format: {with:/[-0-9a-zA-Z.+_]+@[-0-9a-zA-Z.+_]+\.[a-zA-Z]{2,3}/, message: "must be a valid email address" }
   validates :password, length: {minimum: 8, message: "Password must be 8 or more characters", if: 'password.present?'}
 
 
