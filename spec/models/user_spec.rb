5100d6a9ff5b62378b2f42d080d3c582c6f6ba1c
Aubrey <simplyaubs@gmail.com>
2014-05-20 10:25:04 -0600

    [#71632410] Fixed bug for duplication of tests between feature and models for user registrations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,6 +8,10 @@ describe User do
       expect(@user).to_not be_valid
       expect(@user).to have(2).error_on(:email)
     end
+    it 'should be valid with a valid email address' do
+      user = User.new email: "whatever@gmail.com", password: 'password', password_confirmation: 'password'
+      user.should be_valid
+    end
 
     it "should not be valid without a password" do
       @user = User.new email: "whatever@gmail.com", password: '', password_confirmation: ''
@@ -28,7 +32,13 @@ describe User do
     it "should be valid with a password of 8 characters " do
       user = User.new email: "email@example.com", password: 'shortshort', password_confirmation: 'shortshort'
       user.should be_valid
-      end
+    end
+    it "should not be valid with a duplicate email" do
+      User.create email: "email@example.com", password: 'shortshort', password_confirmation: 'shortshort'
+      user2 = User.new email: "email@example.com", password: 'shortshort', password_confirmation: 'shortshort'
+      user2.should_not be_valid
+      
+    end
   end
 end
 
