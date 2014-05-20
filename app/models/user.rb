b056a9b7a552719693d8a482de8b0588e6cd80ce
Zach Klabunde <zbunde@gmail.com>
2014-05-20 16:12:49 -0600

    [#71418508] took out duplicate password from validation message for email length

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
 
   validates :email, :presence => true, :uniqueness => true
   validates :email, format: {with:/[-0-9a-zA-Z.+_]+@[-0-9a-zA-Z.+_]+\.[a-zA-Z]{2,3}/, message: "must be a valid email address" }
-  validates :password, length: {minimum: 8, message: "Password must be 8 or more characters", if: 'password.present?'}
+  validates :password, length: {minimum: 8, message: "must be 8 or more characters", if: 'password.present?'}
 
 
 end
