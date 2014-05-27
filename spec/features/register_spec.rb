b8c14cd4ea073f1151accd19d005aa61622d21ea
Zach Klabunde <zbunde@gmail.com>
2014-05-27 15:13:43 -0600

    updated spec to include the terms of service

--- spec/features/register_spec.rb
+++ spec/features/register_spec.rb
@@ -14,21 +14,6 @@ feature 'Registering an account' do
     expect(page).to have_content 'Logout'
   end
 
-  scenario 'a guest can logout' do
-    visit '/'
-    click_on 'Register'
-    fill_in 'user[email]', :with => 'email@email.com'
-    fill_in 'user[password]', :with => 'password'
-    fill_in 'user[password_confirmation]', :with => 'password'
-    check 'terms'
-    click_button 'Register'
-    expect(page).to have_content 'Welcome to the newsletter application'
-    expect(page).to have_content 'email@email.com'
-    click_on 'Logout'
-    expect(page).to_not have_content 'email@email.com'
-  end
-
-
   scenario 'email cannot be blank when trying to register' do
     visit '/'
     click_on 'Register'
